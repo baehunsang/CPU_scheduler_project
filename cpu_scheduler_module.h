d60b9e72411f41ba9e257a9b235f9946534903f1
hunsang <hoonsang99@korea.ac.kr>
2023-05-19 17:16:17 +0900

    13. REFACTORING: make scheduling routine to func

--- cpu_scheduler_module.h
+++ cpu_scheduler_module.h
@@ -82,65 +82,38 @@ int is_terminal(cpu_scheduler_ptr cpu_scheduler_addr){
     return terminal;
 }
 
-void __schedule_fcfs(cpu_scheduler_ptr cpu_scheduler_addr){
-    int current_time = 0;
-    int start_time = 0;
-    //1 while roop represents 1ms
-    /*
-        while current time is JQ.HeapMax()->arrival time
-            1.dequeue process from JQ
-            2.insert process into ready Q
-
-        if core is NULL
-            1.dequeue process from ready Q
-            2.schedule core to process
-
-        if running process's io_timer == 0
-            1.change process's is_io field to TRUE
-            2. insert process into log_list
-            if ready Q's size is not 0
-                1.dequeue process from ready Q
-                2.schedule core to process
-            else
-                core = NULL
-
-        if running process's remaining_cpu_burst == 0
-            1. insert process into log_list
-            2. change process's is_end field to TRUE
-        
-        if io_process's remaining_io == 0
-            1. insert process into ready Q
-            2. change process's is_io field to FALSE
-
-        io_process's remaining_io--
-        running process's remaining_cpu_burst--
-        running process's io_timer--
-        
-        
-    */
-    while(!is_terminal(cpu_scheduler_addr)){
-        /*
+/*
         while current time is JQ.HeapMax()->arrival time
             1.dequeue process from JQ
             2.insert process into ready Q
         */
-        while(cpu_scheduler_addr->job_queue->size && current_time == HeapMax(cpu_scheduler_addr->job_queue)->arrival_time){
-            process_ptr dequeued_process = HeapExtractMax(cpu_scheduler_addr->job_queue);
-            insert_to_ready_Q(cpu_scheduler_addr->ready_queue, dequeued_process);
-        }
-        /*
+void move_jq_to_readyQ(cpu_scheduler_ptr cpu_scheduler_addr, int current_time){
+    /*
+        while current time is JQ.HeapMax()->arrival time
+            1.dequeue process from JQ
+            2.insert process into ready Q
+        */
+    while(cpu_scheduler_addr->job_queue->size && current_time == HeapMax(cpu_scheduler_addr->job_queue)->arrival_time){
+        process_ptr dequeued_process = HeapExtractMax(cpu_scheduler_addr->job_queue);
+        insert_to_ready_Q(cpu_scheduler_addr->ready_queue, dequeued_process);
+    }
+}
+
+/*
         if core is NULL
             1.dequeue process from ready Q
             2.schedule core to process
             3. is run field changed to TRUE
         */
-        if(!cpu_scheduler_addr->core && cpu_scheduler_addr->ready_queue->size){
-            cpu_scheduler_addr->core = dequeue_from_ready_Q(cpu_scheduler_addr->ready_queue);
-            cpu_scheduler_addr->core->is_run = TRUE;
-            start_time = current_time;
-        }
+void schedule_if_core_is_null(cpu_scheduler_ptr cpu_scheduler_addr, int current_time, int* start_time){
+    if(!cpu_scheduler_addr->core && cpu_scheduler_addr->ready_queue->size){
+        cpu_scheduler_addr->core = dequeue_from_ready_Q(cpu_scheduler_addr->ready_queue);
+        cpu_scheduler_addr->core->is_run = TRUE;
+        *start_time = current_time;
+    }
+}
 
-        /*
+/*
         if running process's io_timer == 0
             1.change process's is_io field to TRUE is_run field to FALSE
             2. insert process into log_list
@@ -150,55 +123,78 @@ void __schedule_fcfs(cpu_scheduler_ptr cpu_scheduler_addr){
             else
                 core = NULL
         */
-        if(cpu_scheduler_addr->core && cpu_scheduler_addr->core->io_timer == 0){
-            cpu_scheduler_addr->core->is_io = TRUE;
-            cpu_scheduler_addr->core->is_run = FALSE;
-            cpu_scheduler_addr->core->io_timer = MAX_BURST;
-            insert_log(cpu_scheduler_addr->log, cpu_scheduler_addr->core, start_time, current_time);
-            if(cpu_scheduler_addr->ready_queue->size){
-                cpu_scheduler_addr->core = dequeue_from_ready_Q(cpu_scheduler_addr->ready_queue);
-                cpu_scheduler_addr->core->is_run = TRUE;
-                start_time = current_time;
-            }
-            else{
-                cpu_scheduler_addr->core = NULL;
-            }
+void manage_io_occurence(cpu_scheduler_ptr cpu_scheduler_addr, int current_time, int* start_time){
+    if(cpu_scheduler_addr->core && cpu_scheduler_addr->core->io_timer == 0){
+        cpu_scheduler_addr->core->is_io = TRUE;
+        cpu_scheduler_addr->core->is_run = FALSE;
+        cpu_scheduler_addr->core->io_timer = MAX_BURST;
+        insert_log(cpu_scheduler_addr->log, cpu_scheduler_addr->core, *start_time, current_time);
+        if(cpu_scheduler_addr->ready_queue->size){
+            cpu_scheduler_addr->core = dequeue_from_ready_Q(cpu_scheduler_addr->ready_queue);
+            cpu_scheduler_addr->core->is_run = TRUE;
+            *start_time = current_time;
         }
-        /*
+        else{
+            cpu_scheduler_addr->core = NULL;
+        }
+    }
+}
+
+/*
         if running process's remaining_cpu_burst == 0
             1. insert process into log_list
             2. change process's is_end field to TRUE
         */
-        if(cpu_scheduler_addr->core && cpu_scheduler_addr->core->remaining_cpu_time == 0){
-            insert_log(cpu_scheduler_addr->log, cpu_scheduler_addr->core, start_time, current_time);
-            cpu_scheduler_addr->core->is_end = TRUE;
-            cpu_scheduler_addr->core->is_run = FALSE;
-            cpu_scheduler_addr->core->TT += current_time;
-            if(cpu_scheduler_addr->ready_queue->size){
-                cpu_scheduler_addr->core = dequeue_from_ready_Q(cpu_scheduler_addr->ready_queue);
-                cpu_scheduler_addr->core->is_run = TRUE;
-                start_time = current_time;
-            }
-            else{
-                cpu_scheduler_addr->core = NULL;
-            }
+void manage_process_ending_condition(cpu_scheduler_ptr cpu_scheduler_addr, int current_time, int* start_time){
+    if(cpu_scheduler_addr->core && cpu_scheduler_addr->core->remaining_cpu_time == 0){
+        insert_log(cpu_scheduler_addr->log, cpu_scheduler_addr->core, *start_time, current_time);
+        cpu_scheduler_addr->core->is_end = TRUE;
+        cpu_scheduler_addr->core->is_run = FALSE;
+        cpu_scheduler_addr->core->TT += current_time;
+        if(cpu_scheduler_addr->ready_queue->size){
+            cpu_scheduler_addr->core = dequeue_from_ready_Q(cpu_scheduler_addr->ready_queue);
+            cpu_scheduler_addr->core->is_run = TRUE;
+            *start_time = current_time;
         }
-        /*
+        else{
+            cpu_scheduler_addr->core = NULL;
+        }
+        }
+}
+
+/*
         if io_process's remaining_io == 0
             1. insert process into ready Q
             2. change process's is_io field to FALSE
         */
-        for(int i=0; i< PROC_NUM; i++){
-            if(cpu_scheduler_addr->trace_process[i]->is_io && !cpu_scheduler_addr->trace_process[i]->remaining_io_time){
-                cpu_scheduler_addr->trace_process[i]->is_io = FALSE;
-                insert_to_ready_Q(cpu_scheduler_addr->ready_queue, cpu_scheduler_addr->trace_process[i]);
-                if(!cpu_scheduler_addr->core && cpu_scheduler_addr->ready_queue->size){
-                    cpu_scheduler_addr->core = dequeue_from_ready_Q(cpu_scheduler_addr->ready_queue);
-                    cpu_scheduler_addr->core->is_run = TRUE;
-                    start_time = current_time;
-                }
+void manage_io_ending_condition(cpu_scheduler_ptr cpu_scheduler_addr, int current_time, int* start_time){
+    for(int i=0; i< PROC_NUM; i++){
+        if(cpu_scheduler_addr->trace_process[i]->is_io && !cpu_scheduler_addr->trace_process[i]->remaining_io_time){
+            cpu_scheduler_addr->trace_process[i]->is_io = FALSE;
+            insert_to_ready_Q(cpu_scheduler_addr->ready_queue, cpu_scheduler_addr->trace_process[i]);
+            if(!cpu_scheduler_addr->core && cpu_scheduler_addr->ready_queue->size){
+                cpu_scheduler_addr->core = dequeue_from_ready_Q(cpu_scheduler_addr->ready_queue);
+                cpu_scheduler_addr->core->is_run = TRUE;
+                *start_time = current_time;
             }
         }
+    }
+}
+
+void __schedule_fcfs(cpu_scheduler_ptr cpu_scheduler_addr){
+    int current_time = 0;
+    int start_time = 0;
+    while(!is_terminal(cpu_scheduler_addr)){
+        
+        move_jq_to_readyQ(cpu_scheduler_addr, current_time);
+        
+        schedule_if_core_is_null(cpu_scheduler_addr, current_time, &start_time);
+
+        manage_io_occurence(cpu_scheduler_addr, current_time, &start_time);
+        
+        manage_process_ending_condition(cpu_scheduler_addr, current_time, &start_time);
+        
+        manage_io_ending_condition(cpu_scheduler_addr, current_time, &start_time);
 
         for(int i=0; i< PROC_NUM; i++){
             if(cpu_scheduler_addr->trace_process[i]->is_io){
